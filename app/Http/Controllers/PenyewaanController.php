24d8eb0b3ab925c74343aee42aafa23cc6099561
oche <haris.gumilar@sekolahmu.co.id>
2025-01-31 20:11:27 +0700

    fixing store data penyewaan

--- app/Http/Controllers/PenyewaanController.php
+++ app/Http/Controllers/PenyewaanController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Penyewaan;
 use App\Models\Lapangan;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Nette\Utils\Strings;
 
 class PenyewaanController extends Controller
 {
@@ -13,7 +16,7 @@ class PenyewaanController extends Controller
         $penyewaan = Penyewaan::all();
         return view('penyewaan.index', compact('penyewaan'));
     }
-    
+
     public function create()
     {
         $lapangan = Lapangan::all(); // Ambil semua data lapangan
@@ -24,20 +27,27 @@ class PenyewaanController extends Controller
     {
         // Validasi inputan jika perlu
         $request->validate([
-            'user_id' => 'required',
             'lapangan_id' => 'required',
-            'waktu_mulai' => 'required|date',
-            'waktu_selesai' => 'required|date',
-            'total_harga' => 'required|integer',
+            // 'waktu_mulai' => 'required|date',
+            // 'waktu_selesai' => 'required|date',
+            'total_harga' => 'required',
         ]);
 
         // Simpan data penyewaan ke dalam tabel penyewaan
+        // Remove the currency symbol (Rp) and dot
+        $numericTotalHarga = str_replace(['Rp.', '.'], '', $request->total_harga);
+
+        // Convert the string to a numeric value
+        $numericTotalHarga = (float) $numericTotalHarga;
+        // $waktu_mulai = Carbon::parse($request->waktu_mulai);
         Penyewaan::create([
-            'user_id' => $request->user_id,
+            'user_id' => Auth::user()->id,
             'lapangan_id' => $request->lapangan_id,
-            'waktu_mulai' => $request->waktu_mulai,
-            'waktu_selesai' => $request->waktu_selesai,
-            'total_harga' => $request->total_harga,
+            'waktu_mulai' => now(),
+            'waktu_selesai' => now(),
+            // 'waktu_mulai' => $waktu_mulai,
+            // 'waktu_selesai' => $waktu_mulai->clone()->addHours((int)$request->durasi),
+            'total_harga' => $numericTotalHarga,
         ]);
 
         return redirect()->route('penyewaan.index')->with('success', 'Penyewaan berhasil disimpan');
